9576eeda9fa95d0bf514af7398cfa5302155f5be
ZETALVX <lyuk94@hotmail.it>
2026-05-09 03:20:35 +0200

    Delete Zetalvx-Image-to-3D directory

--- Zetalvx-Image-to-3D/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Zetalvx_z3v_hy3d_node import Z3VHy3DFromImage
-
-NODE_CLASS_MAPPINGS = {
-    "Z3VHy3DFromImage": Z3VHy3DFromImage,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Z3VHy3DFromImage": "ZetaLvx - Z3V V.1 - Image to 3D",
-}
